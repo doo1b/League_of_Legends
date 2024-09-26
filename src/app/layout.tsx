3b4d3ed94e920d13faa59d70f6f681b335640ddd
doo1b <tngml928@gmail.com>
2024-09-26 14:29:08 +0900

    🎉 init : 프로젝트 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header>Layout</header>
         {children}
       </body>
     </html>
